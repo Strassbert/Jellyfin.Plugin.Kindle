e6eed91840d3ca07701e0dceac898207989f7808
Strassbert <strassertimo01@gmail.com>
2026-02-13 22:21:54 +0100

    1
    1

--- obj/Release/net9.0/Jellyfin.Plugin.Kindle.AssemblyInfo.cs
+++ obj/Release/net9.0/Jellyfin.Plugin.Kindle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jellyfin.Plugin.Kindle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+53240752071865da229a147624a1a9ed8e6e7078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+f63362928fa7dc56c7df5f5531d5d951f6a14c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jellyfin.Plugin.Kindle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jellyfin.Plugin.Kindle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
